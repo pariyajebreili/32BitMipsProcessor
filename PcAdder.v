d2e41222b964494ef65c8d378b5e15ef95412aae
Pariya <87968419+pariyajebreili@users.noreply.github.com>
2022-06-09 12:54:16 +0430

    Add files via upload

---
+++ PcAdder.v
@@ -0,0 +1,13 @@
+module PcAdder(PcNext, ShiftOut, AddAluOut);
+
+	input [31:0] PcNext;
+	input [31:0] ShiftOut;
+	
+	output reg [31:0] AddAluOut;
+
+	always @(*) 
+	begin
+		AddAluOut <= PcNext + ShiftOut;
+	end
+      
+endmodule
