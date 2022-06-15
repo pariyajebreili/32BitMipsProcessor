c916e3f78a63d12cc1fa17ff723d9ea83e59477e
pariya <pariyajebreili@gmail.com>
2022-06-15 18:15:20 +0430

    .

--- Mips.v
+++ Mips.v
@@ -62,32 +62,4 @@ module Mips(clk, rst, pc ,Instruction, ReadData1, ReadData2,  WriteDataReg,
 
 endmodule
 
-
-module testbech();
-
-      reg clk,rst;
-      wire pc,Instruction,WriteReg,ReadData1, ReadData2,
-      ALUOperation,ALUSrc,Zero,ReadData,WriteDataReg;
       
-/*
-      Mips UUT(clk, rst, pc,Instruction,WriteReg,ReadData1, ReadData2,
-      ALUOperation,ALUSrc,Zero,ReadData,WriteDataReg);
-*/
-      initial begin
-            rst = 1;
-            clk = 0;
-            rst = 0;
-            #100;
-            clk = 1;
-            #100;
-            clk = 0;
-            #100;
-            rst = 1;
-            #100;
-            rst = 0;
-            #100;
-            clk = 1;
-            #100;
-      end
-
-endmodule
